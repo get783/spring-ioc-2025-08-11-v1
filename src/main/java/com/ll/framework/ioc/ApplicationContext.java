5ecee16f8b637e5d8edac0111023a3bed5af0cba
leejiyeon <sky75013@naver.com>
2025-08-19 17:06:19 +0900

    testPostRepository

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,26 @@
 package com.ll.framework.ioc;
 
-public class ApplicationContext {
-    public ApplicationContext() {
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class ApplicationContext {
+    private Map<String, Object> beans;
+
+    public ApplicationContext() {
+        beans = new HashMap<>();
     }
 
     public <T> T genBean(String beanName) {
-        return (T) null;
+        Object bean = beans.get(beanName);
+        if (bean == null) {
+            bean = switch (beanName) {
+                case "testPostRepository" -> new TestPostRepository();
+                default -> null;
+            };
+            beans.put(beanName, bean);
+        }
+        return (T) bean;
     }
 }
